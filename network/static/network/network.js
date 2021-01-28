ee6909c9bd5b07609dcfe0d2e7b541886823fd8a
Pearce Allen <pearce.p.allen@gmail.com>
2021-01-27 21:31:00 -0500

    Fixed new_form js function

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#new_post').addEventListener('submit', (event) => {
-        event.preventDefault()
-        fetch('/new_post',{
-            method: 'POST',
-            body: JSON.stringify({
-                content: document.querySelector('#post').value
-            })
-        })
-        .then(response => response.json())
-        .then(result => {
-            // Print result
-            console.log(result);
-        });
-    });
+    document.querySelector('#new_post').addEventListener('submit', new_form);
 });
+
+function new_form() {
+    fetch('/new_post',{
+        method: 'POST',
+        body: JSON.stringify({
+            content: document.querySelector('#post').value
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+}
 
