8ab656c5a8c4696ab4fb4f02a191000029196618
Pearce Allen <pearce.p.allen@gmail.com>
2021-01-28 22:05:21 -0500

    Rendered all post | no styling

--- network/static/network/post.js
+++ network/static/network/post.js
@@ -9,6 +9,15 @@ function posts() {
         .then(post => {
             // Print posts
             console.log(post);
+
+            post.forEach(p => {
+                const post_info = document.createElement('div');
+                post_info.innerHTML = ('<div>' + `User: ${p.username}` + '</div>' 
+                                        + '<div>' + `${p.content}` + '</div>' 
+                                        + '<div>' + `${p.timestamp}` + '</div>'
+                                        + '<div>' + `Likes: ${p.like}` + '</div>');
+                document.querySelector('#posts').append(post_info);
+            })
         })
 }
 
