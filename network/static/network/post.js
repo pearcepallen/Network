2411ce31df6cef0e386c091f0c2b0dca891a9a78
Pearce Allen <pearce.p.allen@gmail.com>
2021-02-02 20:38:05 -0500

    Render user content

--- network/static/network/post.js
+++ network/static/network/post.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
 });
 
+
 function posts() {
     // Load Posts
     fetch('/posts')
@@ -19,52 +20,10 @@ function posts() {
             // Print posts
             console.log(post);
 
-            post.forEach(p => {
-                // Add User div and link
-                const user = document.createElement('div');
-                user.className = 'user';
-                //user.innerHTML = `${p.username}`;
-                const a = document.createElement('a');
-                a.innerHTML = `${p.username}`;
-                a.href = `/profile/${p.username}`;
-                user.append(a);
-
-                //Add content div
-                const content = document.createElement('div');
-                content.className = 'user';
-                content.innerHTML = `${p.content}`;
-                content.addEventListener('click', function() {
-                    console.log(`${p.content} was clicked`);
-                });
-
-                //Add time div
-                const time = document.createElement('div');
-                time.className = 'user';
-                time.innerHTML = `${p.timestamp}`;
-                time.addEventListener('click', function() {
-                    console.log(`${p.timestamp} was clicked`);
-                });
-
-                //Add likes div
-                const like = document.createElement('div');
-                like.className = 'user';
-                like.innerHTML = `Likes: ${p.like}`;
-                like.addEventListener('click', function() {
-                    console.log(`Like ${p.like} was clicked`);
-                });
-
-                //innerHTML destroys child elements
-                // Add a post div with previous children divs
-                const post_info = document.createElement('div');
-                post_info.className = "single_post";
-                post_info.append(user);
-                post_info.append(content);
-                post_info.append(time);
-                post_info.append(like);
-                document.querySelector('#posts').append(post_info);
-            })
+            load_posts(post, '#posts');
         })
 }
+
 
 function profile(user) {
     // Load user posts
@@ -72,5 +31,46 @@ function profile(user) {
         .then(response => response.json())
         .then(posts => {    
             console.log(posts);
+
+            load_posts(posts, '#profile');
         });
+}
+
+
+function load_posts(post, page) {
+    post.forEach(p => {
+        // Add User div and link
+        const user = document.createElement('div');
+        user.className = 'user';
+        //Add a tag
+        const a = document.createElement('a');
+        a.innerHTML = `${p.username}`;
+        a.href = `/profile/${p.username}`;
+        user.append(a);
+
+        //Add content div
+        const content = document.createElement('div');
+        content.className = 'user';
+        content.innerHTML = `${p.content}`;
+
+        //Add time div
+        const time = document.createElement('div');
+        time.className = 'user';
+        time.innerHTML = `${p.timestamp}`;
+
+        //Add likes div
+        const like = document.createElement('div');
+        like.className = 'user';
+        like.innerHTML = `Likes: ${p.like}`;
+    
+        //innerHTML destroys child elements
+        // Add a post div with previous children divs
+        const post_info = document.createElement('div');
+        post_info.className = "single_post";
+        post_info.append(user);
+        post_info.append(content);
+        post_info.append(time);
+        post_info.append(like);
+        document.querySelector(page).append(post_info);
+    })
 }
